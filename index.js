bed42f62068b608bd9c06143db0152a32f777598
DavideLai <davidelai21@yahoo.it>
2023-11-27 12:32:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+let myH1 = document.getElementById("E-store");
+myH1.innerText= "Sono il nuovo contenuto di H1!";
+*/
+
+/*
+document.body.style.backgroundColor="rgba(204, 219, 219, 0.24)";
+*/
+
+/*
+let pFooter = document.getElementById("p-footer");
+pFooter.innerText="Indirizzo fittizio 100, Città fittizia, no cap";
+*/
+
+/*
+let newClass= document.getElementById("h4");
+newClass.classList.add("new-class-h4");
+*/
+
+
+
+
